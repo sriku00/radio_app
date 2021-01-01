aaa5d03925ee523097f12080e3b6748cf2d08cfd
s <51485083+sriku00@users.noreply.github.com>
2021-01-01 14:45:01 +0530

    yes

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,9 +1,12 @@
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text("hello"),
+      ),
+    );
   }
 }
