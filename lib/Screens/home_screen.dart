b88c514af6192274b7d0a3396ddfa23be6af9c3d
s <51485083+sriku00@users.noreply.github.com>
2021-01-01 14:46:41 +0530

    yes

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("hello"),
+        child: Text("nothing"),
       ),
     );
   }
