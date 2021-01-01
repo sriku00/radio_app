8d66c1985a5efd675464ba10b3792e32e6ba26eb
s <51485083+sriku00@users.noreply.github.com>
2021-01-01 14:50:08 +0530

    yes

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Center(
+      child: Text("page One"),
+    ),
+    Center(
+      child: Text("page One"),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
